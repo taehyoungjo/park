6782a4d5e01b34fcd679e0c78fb1dc0539f0c8bc
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2020-05-12 07:44:12 -0400

    in water, basic ledges, broken ball:

--- public/js/game.js
+++ public/js/game.js
@@ -3,8 +3,8 @@ var config = {
   parent: "phaser-example",
   //   width: 800,
   //   height: 600,
-  width: 1152,
-  height: 768,
+  width: 512,
+  height: 288,
   physics: {
     default: "arcade",
     arcade: {
@@ -20,74 +20,87 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var water_filtered;
 
 function preload() {
-  // Temp background
-  // this.load.image("background", "assets/background_temp.png");
-  this.load.image("background", "assets/background.png");
+  // this.load.image("background", "assets/background.png");
 
-  // this.load.image("ship", "assets/slime.png");
-  // this.load.image("ship", "assets/spaceShips_001.png");
+  this.load.image("tiles", "assets/map/tileset.png");
+  this.load.tilemapTiledJSON("map", "assets/map/park.json");
 
   this.load.spritesheet("slime", "assets/slime_frames.png", {
-    frameWidth: 93,
-    frameHeight: 93,
+    frameWidth: 31,
+    frameHeight: 31,
   });
 
-  // this.load.image("otherPlayer", "assets/enemyBlack5.png");
-
-  this.load.spritesheet("otherPlayer", "assets/slime_frames.png", {
-    frameWidth: 93,
-    frameHeight: 93,
-  });
   this.load.image("star", "assets/star_gold.png");
+  this.load.image("ball", "assets/ball.png");
+
+  this.load.spritesheet("splash", "assets/splash.png", {
+    frameWidth: 31,
+    frameHeight: 31,
+  });
 }
 
 function create() {
-  //  A simple background for our game
-  // this.add.image(192, 128, "background");
-  this.add.image(576, 384, "background");
+  // this.add.image(576, 384, "background");
 
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("slime", { start: 0, end: 4 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+  const map = this.make.tilemap({ key: "map" });
 
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("slime", { start: 5, end: 9 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+  // need more descriptive name for tileset
+  const tileset = map.addTilesetImage("tileset", "tiles");
+  const ground = map.createStaticLayer("Ground", tileset, 0, 0);
+  const water = map.createDynamicLayer("Water", tileset, 0, 0);
+  const skyBarrier = map.createStaticLayer("Sky_barrier", tileset, 0, 0);
 
-  this.anims.create({
-    key: "up",
-    frames: this.anims.generateFrameNumbers("slime", { start: 10, end: 14 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+  skyBarrier.setCollisionByProperty({ collide: true });
 
-  this.anims.create({
-    key: "down",
-    frames: this.anims.generateFrameNumbers("slime", { start: 15, end: 19 }),
-    frameRate: 10,
-    repeat: -1,
+  //   const waterObjects = map.getObjectLayer("Water")["objects"];
+  //   waterObjects.forEach((waterObject) => {
+  //     const water = this.water
+  //       .create(waterObject.x, waterObject.y + 200 - waterObject.height, tileset)
+  //       .setOrigin(0, 0);
+  //   });
+
+  // I can make four layers and test for overlap between any of the four
+
+  //   this.anims.create({
+  //     key: "splash",
+  //     frames: this.anims.generateFrameNumbers("splash", { start: 0, end: 0 }),
+  //     frameRate: 10,
+  //     repeat: -1,
+  //   });
+
+  createAnimations(this);
+
+  water_filtered = water.filterTiles((t) => {
+    return t.properties.collides;
   });
 
   var self = this;
   this.socket = io();
+
+  // CHECK
   this.otherPlayers = this.physics.add.group();
+
   this.socket.on("currentPlayers", function (players) {
     Object.keys(players).forEach(function (id) {
       if (players[id].playerId === self.socket.id) {
         addPlayer(self, players[id]);
+        self.physics.add.collider(self.container, skyBarrier);
       } else {
         addOtherPlayers(self, players[id]);
       }
     });
   });
+
+  const debugGraphics = this.add.graphics().setAlpha(0.75);
+  skyBarrier.renderDebug(debugGraphics, {
+    tileColor: null, // Color of non-colliding tiles
+    collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+    faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
+  });
+
   this.socket.on("newPlayer", function (playerInfo) {
     addOtherPlayers(self, playerInfo);
   });
@@ -100,25 +113,32 @@ function create() {
   });
 
   this.socket.on("playerMoved", function (playerInfo) {
+    // console.log(self.otherPlayers);
+    // console.log(self.otherPlayers.getChildren());
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-      if (playerInfo.playerId === otherPlayer.playerId) {
-        // otherPlayer.setRotation(playerInfo.rotation);
-        console.log(playerInfo.rotation);
+      // console.log(otherPlayer.list[0].playerId);
+      if (playerInfo.playerId === otherPlayer.list[0].playerId) {
+        // console.log(playerInfo.inWater);
+        if (playerInfo.inWater) {
+          otherPlayer.list[1].anims.play("splash", true);
+        } else {
+          otherPlayer.list[1].anims.play("nosplash", true);
+        }
         switch (playerInfo.rotation) {
           case 1:
-            otherPlayer.anims.play("up", true);
+            otherPlayer.list[0].anims.play("up", true);
             break;
           case 2:
-            otherPlayer.anims.play("right", true);
+            otherPlayer.list[0].anims.play("right", true);
             break;
           case 3:
-            otherPlayer.anims.play("down", true);
+            otherPlayer.list[0].anims.play("down", true);
             break;
           case 4:
-            otherPlayer.anims.play("left", true);
+            otherPlayer.list[0].anims.play("left", true);
             break;
           default:
-            otherPlayer.anims.play("down", true);
+            otherPlayer.list[0].anims.play("down", true);
         }
         otherPlayer.setPosition(playerInfo.x, playerInfo.y);
       }
@@ -144,7 +164,8 @@ function create() {
     if (self.star) self.star.destroy();
     self.star = self.physics.add.image(starLocation.x, starLocation.y, "star");
     self.physics.add.overlap(
-      self.ship,
+      self.container,
+      //   self.ship,
       self.star,
       function () {
         this.socket.emit("starCollected");
@@ -154,138 +175,227 @@ function create() {
     );
   });
 
+  this.socket.on("ballLocation", function (ballLocation) {
+    if (self.ball) self.ball.destroy();
+    self.ball = self.physics.add.image(ballLocation.x, ballLocation.y, "ball");
+    self.physics.add.collider(self.container, self.ball, (cb) => {
+      console.log(cb);
+    });
+    // self.ball.oldPosition = {
+    //     x: -1,
+    //     y: -1
+    // }
+  });
+
+  this.socket.on("ballMoved", function (ballInfo) {
+    self.ball.setPosition(ballInfo.x, ballInfo.y);
+  });
+
   this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  if (this.ship) {
-    var r;
+  //   if (this.ball) {
+  //     // emit player movement
+  //     var x = this.ball.x;
+  //     var y = this.ball.y;
+  //     if (
+  //       this.ball.oldPosition &&
+  //       (x !== this.ball.oldPosition.x || y !== this.ball.oldPosition.y)
+  //     ) {
+  //     this.socket.emit("ballMovement", {
+  //       x: x,
+  //       y: y,
+  //       // inWater: inWater,
+  //     });
+  //     // }
 
-    if (this.cursors.left.isDown) {
-      this.ship.setVelocityX(-160);
+  //     // // save old position data
+  //     // this.ball.oldPosition = {
+  //     //   x: x,
+  //     //   y: y,
+  //     //   // inWater: inWater,
+  //     // };
+  //   }
 
-      if (this.cursors.up.isDown) {
-        this.ship.setVelocityY(-160);
-        this.ship.anims.play("up", true);
-        r = 1;
-      } else if (this.cursors.down.isDown) {
-        this.ship.setVelocityY(160);
-        this.ship.anims.play("left", true);
-        r = 4;
-      } else {
-        this.ship.setVelocityY(0);
-        this.ship.anims.play("left", true);
-        r = 4;
-      }
-    } else if (this.cursors.right.isDown) {
-      this.ship.setVelocityX(160);
-
-      if (this.cursors.up.isDown) {
-        this.ship.setVelocityY(-160);
-        this.ship.anims.play("up", true);
-        r = 1;
-      } else if (this.cursors.down.isDown) {
-        this.ship.setVelocityY(160);
-        this.ship.anims.play("right", true);
-        r = 2;
-      } else {
-        this.ship.setVelocityY(0);
-        this.ship.anims.play("right", true);
-        r = 2;
-      }
+  if (this.container) {
+    var inWater;
+    var p = this.physics.world.overlapTiles(
+      this.container,
+      water_filtered,
+      null,
+      null,
+      this
+    );
+    if (p) {
+      this.splash.anims.play("splash", true);
+      inWater = true;
     } else {
-      this.ship.setVelocityX(0);
-
-      if (this.cursors.up.isDown) {
-        this.ship.setVelocityY(-160);
-        this.ship.anims.play("up", true);
-        r = 1;
-      } else if (this.cursors.down.isDown) {
-        this.ship.setVelocityY(160);
-        this.ship.anims.play("down", true);
-        r = 3;
-      } else {
-        this.ship.setVelocityY(0);
-        // this.ship.anims.play("down", true);
-      }
+      this.splash.anims.play("nosplash", true);
+      // this.splash.anims.remove();
+      inWater = false;
     }
 
-    // if (this.cursors.left.isDown && this.cursors.up.isDown)
+    this.container.body.setVelocity(0);
 
-    // World wrap is broken
-    // this.physics.world.wrap(this.ship, 5);
+    var r;
+    if (this.cursors.left.isDown) {
+      this.container.body.setVelocityX(-80);
+    } else if (this.cursors.right.isDown) {
+      this.container.body.setVelocityX(80);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.container.body.setVelocityY(-80);
+    } else if (this.cursors.down.isDown) {
+      this.container.body.setVelocityY(80);
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.anims.play("left", true);
+      r = 4;
+    } else if (this.cursors.right.isDown) {
+      this.player.anims.play("right", true);
+      r = 2;
+    } else if (this.cursors.up.isDown) {
+      this.player.anims.play("up", true);
+      r = 1;
+    } else if (this.cursors.down.isDown) {
+      this.player.anims.play("down", true);
+      r = 3;
+    }
 
     // WE SHOULD BE EMITTING DIRECTION FACING
-    // 1 1 1
+    // 4 1 2
     // 4   2
     // 4 3 2
     // THESE CAN BE THE DIRECTIONS FOR NOW
 
     // emit player movement
-    var x = this.ship.x;
-    var y = this.ship.y;
-    // var r = this.ship.rotation;
+    var x = this.container.x;
+    var y = this.container.y;
     if (
-      this.ship.oldPosition &&
-      (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y)
+      this.container.oldPosition &&
+      (x !== this.container.oldPosition.x || y !== this.container.oldPosition.y)
     ) {
       this.socket.emit("playerMovement", {
-        x: this.ship.x,
-        y: this.ship.y,
+        x: x,
+        y: y,
         rotation: r
           ? r
-          : this.ship.oldPosition
-          ? this.ship.oldPosition.rotation
+          : this.container.oldPosition
+          ? this.container.oldPosition.rotation
           : 3,
-        // rotation: this.ship.rotation,
+        inWater: inWater,
       });
     }
 
     // save old position data
-    this.ship.oldPosition = {
-      x: this.ship.x,
-      y: this.ship.y,
+    this.container.oldPosition = {
+      x: x,
+      y: y,
       rotation: r
         ? r
-        : this.ship.oldPosition
-        ? this.ship.oldPosition.rotation
+        : this.container.oldPosition
+        ? this.container.oldPosition.rotation
         : 3,
-      // rotation: this.ship.rotation,
+      inWater: inWater,
     };
   }
 }
 
 function addPlayer(self, playerInfo) {
-  self.ship = self.physics.add
-    .sprite(playerInfo.x, playerInfo.y, "slime")
-    .setOrigin(0.5, 0.5)
-    .setDisplaySize(93, 93);
-  // .setDisplaySize(53, 40);
-  if (playerInfo.team === "blue") {
-    // self.ship.setTint(0x0000ff);
-  } else {
-    // self.ship.setTint(0xff0000);
-  }
-  self.ship.anims.play("down", true);
-  self.ship.setCollideWorldBounds(true);
-  //   self.ship.setDrag(100);
-  //   self.ship.setAngularDrag(100);
-  //   self.ship.setMaxVelocity(200);
+  self.player = self.add.sprite(0, 0, "slime", 0);
+
+  self.container = self.add.container(playerInfo.x, playerInfo.y);
+  self.container.setSize(31, 31);
+  self.physics.world.enable(self.container);
+  self.container.add(self.player);
+
+  self.splash = self.add.sprite(0, 0, "splash", 2);
+  self.splash.setSize(31, 31);
+  // self.physics.world.enable(self.splash);
+
+  self.container.add(self.splash);
+
+  self.container.body.setCollideWorldBounds(true);
+
+  self.player.anims.play("down", true);
 }
 
 function addOtherPlayers(self, playerInfo) {
-  const otherPlayer = self.add
-    .sprite(playerInfo.x, playerInfo.y, "otherPlayer")
-    .setOrigin(0.5, 0.5)
-    .setDisplaySize(93, 93);
-  if (playerInfo.team === "blue") {
-    otherPlayer.setTint(0x0000ff);
-  } else {
-    otherPlayer.setTint(0xff0000);
-  }
+  var otherPlayer = self.add.sprite(0, 0, "slime", 0);
   otherPlayer.playerId = playerInfo.playerId;
 
-  otherPlayer.anims.play("down", true);
-  self.otherPlayers.add(otherPlayer);
-  console.log(self.otherPlayers);
+  otherPlayer.anims.play("down");
+
+  var container = self.add.container(playerInfo.x, playerInfo.y);
+  container.setSize(31, 31);
+  container.add(otherPlayer);
+
+  self.otherPlayers.add(container);
+
+  var splash = self.add.sprite(0, 0, "splash", 2);
+  splash.setSize(31, 31);
+
+  container.add(splash);
+  // container.anims.play("down", true);
+
+  //   const otherPlayer = self.add
+  //     .sprite(playerInfo.x, playerInfo.y, "slime")
+  //     .setOrigin(0.5, 0.5)
+  //     .setDisplaySize(31, 31);
+  //   //   if (playerInfo.team === "blue") {
+  //   //     otherPlayer.setTint(0x0000ff);
+  //   //   } else {
+  //   //     otherPlayer.setTint(0xff0000);
+  //   //   }
+  //   otherPlayer.playerId = playerInfo.playerId;
+
+  //   otherPlayer.anims.play("down", true);
+  //   self.otherPlayers.add(otherPlayer);
+}
+
+function createAnimations(self) {
+  self.anims.create({
+    key: "right",
+    frames: self.anims.generateFrameNumbers("slime", { start: 0, end: 4 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  self.anims.create({
+    key: "left",
+    frames: self.anims.generateFrameNumbers("slime", { start: 5, end: 9 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  self.anims.create({
+    key: "up",
+    frames: self.anims.generateFrameNumbers("slime", { start: 10, end: 14 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  self.anims.create({
+    key: "down",
+    frames: self.anims.generateFrameNumbers("slime", { start: 15, end: 19 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  self.anims.create({
+    key: "splash",
+    frames: self.anims.generateFrameNumbers("splash", { start: 0, end: 0 }),
+    frameRate: 30,
+    repeat: -1,
+  });
+
+  self.anims.create({
+    key: "nosplash",
+    frames: self.anims.generateFrameNumbers("splash", { start: 2, end: 2 }),
+    frameRate: 180,
+    repeat: -1,
+  });
 }
--- server.js
+++ server.js
@@ -4,12 +4,20 @@ var server = require("http").Server(app);
 
 var io = require("socket.io").listen(server);
 
+var h = 288;
+var w = 512;
+var m = 32;
+
 var players = {};
 var star = {
-  x: Math.floor(Math.random() * 1052) + 50,
-  y: Math.floor(Math.random() * 380) + 338,
+  x: Math.floor(Math.random() * w - 2 * m) + m,
+  y: Math.floor(Math.random() * h - 2 * m) + 4 * m,
   //   x: Math.floor(Math.random() * 700) + 50,
   //   y: Math.floor(Math.random() * 500) + 50,
+};
+var ball = {
+  x: Math.floor(Math.random() * w - 2 * m) + m,
+  y: Math.floor(Math.random() * h - 2 * m) + 4 * m,
 };
 var scores = {
   blue: 0,
@@ -28,8 +36,9 @@ io.on("connection", function (socket) {
   // create a new player and add it to our players object
   players[socket.id] = {
     rotation: 0,
-    x: Math.floor(Math.random() * 1052) + 50,
-    y: Math.floor(Math.random() * 380) + 338,
+    inWater: false,
+    x: Math.floor(Math.random() * w - 2 * m) + m,
+    y: Math.floor(Math.random() * h - 2 * m) + 4 * m,
     // x: Math.floor(Math.random() * 700) + 50,
     // y: Math.floor(Math.random() * 500) + 50,
     playerId: socket.id,
@@ -41,6 +50,9 @@ io.on("connection", function (socket) {
 
   // send the star object to the new player
   socket.emit("starLocation", star);
+
+  // send the ball object to the new player
+  socket.emit("ballLocation", ball);
 
   // send the current scores
   socket.emit("scoreUpdate", scores);
@@ -62,8 +74,14 @@ io.on("connection", function (socket) {
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
     players[socket.id].rotation = movementData.rotation;
+    players[socket.id].inWater = movementData.inWater;
     // emit a message to all players about the player that moved
     socket.broadcast.emit("playerMoved", players[socket.id]);
+  });
+
+  // when the ball moves
+  socket.on("ballMovement", function (movementData) {
+    socket.broadcast.emit("ballMoved", movementData);
   });
 
   socket.on("starCollected", function () {
@@ -72,10 +90,8 @@ io.on("connection", function (socket) {
     } else {
       scores.blue += 10;
     }
-    star.x = Math.floor(Math.random() * 1052) + 50;
-    star.y = Math.floor(Math.random() * 380) + 338;
-    // star.x = Math.floor(Math.random() * 700) + 50;
-    // star.y = Math.floor(Math.random() * 500) + 50;
+    star.x = Math.floor(Math.random() * w - 2 * m) + m;
+    star.y = Math.floor(Math.random() * h - 2 * m) + 4 * m;
     io.emit("starLocation", star);
     io.emit("scoreUpdate", scores);
   });
